a3e12b6e4bf8bcffb20e939f0a1c25d3ca66bd33
김인중 <injoong.kim@mirero.co.kr>
2024-07-05 13:44:47 +0900

    [MOD] ProperBraket.js 코딩테스트 일부 수정

--- 2.Programing/1.CondingTest/2.Programmers/ProperBraket/ProperBraket.js
+++ 2.Programing/1.CondingTest/2.Programmers/ProperBraket/ProperBraket.js
@@ -1,4 +1,15 @@
-var ret = solution("(())())");
+
+
+//var test = "()()(()(())((())(())";
+//var test = "(()))";
+//var test = "(())())";
+//var test = "(())";
+//var test = ")()("; 
+//var test = "(()))()(";
+var test = "((()()))())";
+
+
+var ret = solution(test);
 
 console.log(ret);
 
@@ -10,29 +21,18 @@ function solution(s)
     if(s.length > 100000)
     {
         return false;
-    }      
-
-    // 문자열 s는 '(' 또는 ')' 
-    for(var i = 0; i < s.length; i++)
+    }   
+    
+    // 시작이 '(' 이고 끝이 ')' 가 아닐 경우 Error
+    if(s[0] != '(' || s[s.length - 1] != ')')
     {
-        if(s[i] == '(' || s[i] == ')')
-        {
-            continue;
-        }
-        else
-        {
-            return false;
-        }
+        return false;
     }
 
     // 문자열의 괄호가 ()로 이루어져 있는지 확인
     var count = 0;  
     for(var i = 0; i < s.length; i++)
-    {
-        if(i == 0 && s[i] != '(')
-        {
-            return false;
-        }
+    {        
         
         if(s[i] == '(')
         {
@@ -42,13 +42,20 @@ function solution(s)
         {
             count--;
         }
+        else
+        {
+            // '(' 또는 ')' 가 아닌 문자가 있을 경우 Error
+            return false;
+        }
     }
 
     if(count != 0)
     {
-        return false;
+        // 괄호의 갯수가 맞지 않을 경우 Error
+        answer = false;
     }   
 
+    console.log(`Count -> ${count}` );
 
     return answer;
 
